25704daf868a398cd7cd015f95ae0cc940ea2212
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-06-09 16:53:17 +0300

    update func

--- fastapi/routes/user.py
+++ fastapi/routes/user.py
@@ -6,3 +6,23 @@ user = APIRouter()
 @user.get("/")
 async def read_data():
     return conn.execute(users.select()).fetchall()
+
+@user.get("/{id}")
+async def read_data(id: int):
+    return conn.execute(users.select().where(users.c.id == id)).fetchall()
+
+@user.post("/")
+async def write_data():
+    return conn.execute(users.insert().values(
+        name = user.name,
+        email = user.email,
+        password = user.password
+        )).fetchall()
+
+@user.get("/")
+async def read_data():
+    return conn.execute(users.select()).fetchall()
+
+@user.get("/")
+async def read_data():
+    return conn.execute(users.select()).fetchall()
